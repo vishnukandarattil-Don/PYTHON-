411b39e72d67a27c45835176e68dac0a68213433
vishnukandarattil-Don <vishnukandarattil@gmail.com>
2026-02-11 21:37:45 +0530

    this is a code for area calculation

--- Helloworld.py
+++ Helloworld.py
@@ -1,2 +1,8 @@
-print("hello world")
-print("hello world")
+print("hello I Vishnu K", ", this is my code on finding area of a circle",
+      "and area of a rectangle")
+# area of a circle
+radius = int(input("enter the radius of the circle"))
+area = print("Area=", 3.14*radius*radius, "sq units", 3.14*radius**2)
+length = int(input(" enter the length of the rectangle="))
+breadth = int(input("enter the breadth of the rectangle="))
+Area = print("Area of rectange=", length*breadth, "sq units")
