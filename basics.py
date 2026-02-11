9c2e9a7091d6882320769f2f66786482905fd607
vishnukandarattil-Don <vishnukandarattil@gmail.com>
2026-02-11 22:25:35 +0530

    new update

--- basics.py
+++ basics.py
@@ -1,34 +1,34 @@
-# print("hello I Vishnu K", ", this is my code on finding area of a circle",
-#       "and area of a rectangle")
-# # area of a circle
-# radius = int(input("enter the radius of the circle"))
-# area = print("Area=", 3.14*radius*radius, "sq units", 3.14*radius**2)
-# length = int(input(" enter the length of the rectangle="))
-# breadth = int(input("enter the breadth of the rectangle="))
-# Area = print("Area of rectange=", length*breadth, "sq units")
-# name = input("enter your name=")
+print("hello I Vishnu K", ", this is my code on finding area of a circle",
+      "and area of a rectangle")
+# area of a circle
+radius = int(input("enter the radius of the circle"))
+area = print("Area=", 3.14*radius*radius, "sq units", 3.14*radius**2)
+length = int(input(" enter the length of the rectangle="))
+breadth = int(input("enter the breadth of the rectangle="))
+Area = print("Area of rectange=", length*breadth, "sq units")
+name = input("enter your name=")
 
-# # Uage of type function
-# print(type(radius))
-# print(type(length))
-# print(type(breadth))
-# print(type(name))
-
-
-# # Arithmetic operators
-# a = 10
-# b = 20
-# sum = a+b
-# print("the sum of a and b is=", sum)
-# difference = a-b
-# print(a == b)
-# print(a != b)
-# print(a > b)
-# print(a < b)
-# print(a >= b)
+# Uage of type function
+print(type(radius))
+print(type(length))
+print(type(breadth))
+print(type(name))
 
 
-# type conversion
+# Arithmetic operators
+a = 10
+b = 20
+sum = a+b
+print("the sum of a and b is=", sum)
+difference = a-b
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+
+
+type conversion
 c, d = 5, int("20")
 e, f = str(c), str(d)
 print("the value of e and f is=", c, d)
